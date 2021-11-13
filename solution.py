f82e78defd66a6915d10ad8a7b53bb2e0581a377
catcoppola <85591644+catcoppola@users.noreply.github.com>
2021-11-13 11:35:50 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -141,7 +141,7 @@ def get_route(hostname):
                     # Fill in start
                     # You should add your responses to your lists here
                     rtt = str(round((timeReceived - timeSent) * 1000)) + 'ms'
-                    tracelist1.append([str(ttl), rtt, str(srcHostname), addr[0]])
+                    tracelist1.append([str(ttl), rtt, str(addr[0]), srcHostname])
                     tracelist2.append([tracelist1])
 
 
@@ -152,7 +152,7 @@ def get_route(hostname):
                     # Fill in start
                     # You should add your responses to your lists here
                     rtt = "None"
-                    tracelist1.append([str(ttl), rtt, str(srcHostname), addr[0]])
+                    tracelist1.append([str(ttl), rtt, str(addr[0]), srcHostname])
                     tracelist2.append([tracelist1])
 
 
@@ -163,7 +163,7 @@ def get_route(hostname):
                     # Fill in start
                     # You should add your responses to your lists here and return your list if your destination IP is met
                     rtt = str(round((timeReceived - timeSent)) * 1000) + 'ms'
-                    tracelist1.append([str(ttl), rtt, str(srcHostname), addr[0]])
+                    tracelist1.append([str(ttl), rtt, str(addr[0]), srcHostname])
                     tracelist2.append([tracelist1])
 
 
@@ -171,7 +171,7 @@ def get_route(hostname):
                 else:
                 # Fill in start
                 # If there is an exception/error to your if statements, you should append that to your list here
-                    tracelist1.append([str(ttl), 'none', str(srcHostname), addr[0]])
+                    tracelist1.append([str(ttl), 'none', str(addr[0]), srcHostname])
                     tracelist2.append([tracelist1])
 
                 return tracelist2
