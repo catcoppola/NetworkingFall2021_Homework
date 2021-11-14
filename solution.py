d532d988d520147e38b0fb154a0ad8c0353a246d
catcoppola <85591644+catcoppola@users.noreply.github.com>
2021-11-13 22:26:40 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -179,7 +179,7 @@ def get_route(hostname):
                 break
 
             finally:
-                return tracelist2
+                #return tracelist2
                 mySocket.close()
 
-    
+
