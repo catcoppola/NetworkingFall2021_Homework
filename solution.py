22640b7c09d283b701a0bcce345e2cd8267e67fa
catcoppola <85591644+catcoppola@users.noreply.github.com>
2021-11-14 20:29:45 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -102,7 +102,7 @@ def get_route(hostname):
                 whatReady = select.select([mySocket], [], [], timeLeft)
                 howLongInSelect = (time.time() - startedSelect)
                 if whatReady[0] == []:  # Timeout
-                    tracelist1.append([str(ttl), "* * * Request timed out."])
+                    tracelist1.append([str(ttl), '*', "Request timed out", ''])
                     # Fill in start
                     # You should add the list above to your all traces list
                     #tracelist2.append([str(ttl), "Request Timed Out"])
@@ -113,7 +113,7 @@ def get_route(hostname):
                 timeReceived = time.time()
                 timeLeft = timeLeft - howLongInSelect
                 if timeLeft <= 0:
-                    tracelist1.append([str(ttl), "* * * Request timed out."])
+                    tracelist1.append([str(ttl), '*', "Request timed out", ''])
                     # Fill in start
                     # You should add the list above to your all traces list
                     tracelist2.append(tracelist1)
@@ -186,5 +186,3 @@ def get_route(hostname):
                 mySocket.close()
 
     return tracelist2
-
-
